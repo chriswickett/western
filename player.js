7480c2c63036dd9d63bed8b562427e61c70e6ec1
Chris Wickett <chris@chriswickett.com>
2014-11-10 15:22:38 +0000

    Remove old files

--- player.js
+++
@@ -1,4 +0,0 @@
-var player = new Sprite('images/cowboy-front.png', 100, 100);
-
-player.speed = 2;
-player.vector = null;
--- target.js
+++
@@ -1,3 +0,0 @@
-var target = new Sprite('images/cursor-move.png', player.x, player.y);
-
-target.active = false;
